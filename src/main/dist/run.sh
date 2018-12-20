55fddebce783424940decd463bafe23b9832d141
Marek Tutaj <mtutaj@mcw.edu>
2018-12-20 13:38:20 -0600

    updated build.gradle

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -7,8 +7,7 @@ APPNAME=OrthologRelationLoading
 
 APPDIR=/home/rgddata/pipelines/$APPNAME
 cd $APPDIR
-pwd
-DB_OPTS="-Dspring.config=$APPDIR/../properties/default_db.xml"
-LOG4J_OPTS="-Dlog4j.configuration=file://$APPDIR/properties/log4j.properties"
-declare -x "ORTHOLOG_RELATION_LOADING_OPTS=$DB_OPTS $LOG4J_OPTS"
-bin/$APPNAME "$@" 2>&1 | tee run.log
+
+java -Dspring.config=$APPDIR/../properties/default_db.xml \
+    -Dlog4j.configuration=file://$APPDIR/properties/log4j.properties \
+    -jar lib/${APPNAME}.jar "$@" 2>&1 | tee run.log
