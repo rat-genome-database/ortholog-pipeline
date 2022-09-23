e20cf1491f626d3bf082ad6e753b78cc04ca3272
tutajm <mtutaj@mcw.edu>
2022-09-23 12:53:58 -0500

    tweak scripts

--- src/main/dist/loadSpecies.sh
+++ src/main/dist/loadSpecies.sh
@@ -5,13 +5,5 @@
 #
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 HOMEDIR=/home/rgddata/pipelines/OrthologRelationLoading
-EMAIL=mtutaj@mcw.edu
-if [ "$SERVER" = "REED" ]; then
-  EMAIL_LIST=mtutaj@mcw.edu,slaulederkind@mcw.edu
-fi
-SKIP_EMAIL_SUMMARY=0
 
 ${HOMEDIR}/run.sh --species $1 > ${HOMEDIR}/$1.log
-if [ $SKIP_EMAIL_SUMMARY -ne 1 ]; then
-  mailx -s "[$SERVER] RGD orthologs OK for $1" $EMAIL < ${HOMEDIR}/$1.log
-fi
--- src/main/dist/runAll.sh
+++ src/main/dist/runAll.sh
@@ -4,11 +4,8 @@ APPDIR=/home/rgddata/pipelines/OrthologRelationLoading
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 
 #load human-to-species orthologs (only direct orthologs)
-#              "mouse" "rat" "dog" "bonobo" "squirrel" "chinchilla" "pig" "vervet" "molerat" )
-SPECIES_LIST=( "2"     "3"   "6"   "5"      "7"        "4"          "9"   "13"     "14" )
-
-# OLD: unportable: species common name can change
-# SPECIES_LIST=( "mouse" "rat" "dog" "bonobo" "squirrel" "chinchilla" "pig" "vervet" "molerat" )
+#  species name must be exactly as it is in SPECIES_TYPE.SHORT_NAME
+SPECIES_LIST=( "mouse" "rat" "dog" "bonobo" "squirrel" "chinchilla" "pig" "vervet" "molerat" )
 
 export SKIP_EMAIL_SUMMARY=1
 for SPECIES in "${SPECIES_LIST[@]}"; do
